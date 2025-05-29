241c210187e52293127434c713f493488e8bee7c
7t4o <hatemlaptop1@gmail.com>
2025-05-29 19:20:44 +0300

    add animation to the main page to make the background change

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,21 @@
-const burgerMenu = document.querySelector('.menu');
-const header = document.querySelector('header');
+const burgerMenu = document.querySelector(".menu");
+const header = document.querySelector("header");
 
-burgerMenu.addEventListener('click', function() {
-    header.classList.toggle('expanded');
+burgerMenu.addEventListener("click", function () {
+    header.classList.toggle("expanded");
 });
+
+// background animation for main page
+const imageBox = document.getElementById("imageBox");
+let currentIndex = 0;
+
+function changeBackground() {
+    setTimeout(() => {
+        currentIndex = (currentIndex + 1) % 6;
+        console.log(currentIndex)
+        imageBox.style.backgroundImage = `url("../assets/images/background/bg${currentIndex}.jpg")`;
+    }, 1000); 
+}
+
+imageBox.style.backgroundImage = `url("../assets/images/background/bg${currentIndex}.jpg")`;
+setInterval(changeBackground, 5000);
